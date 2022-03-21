5871c359a976f5cd30c89d11212611cb3ae4a099
asunfavour <asunfavour03@gmail.com>
2022-03-21 23:14:22 +0100

    nested loops

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
 {
-    let  myString = "search this string baby";
-    let charToSearch = "c";
-    for(let i = 0; i < myString.length; i++){
-        if (myString[i] === charToSearch){
-            console.log(myString[i] + " is found at index " + i);
+    let d = document.getElementById("destination")
+    for (let i = 0; i < 10; i++){
+        for (let k = i; k >= 0; k--){
+            d.append(k + "");
         }
+        var br = document.createElement('br')
+        d.appendChild(br)
     }
 }
