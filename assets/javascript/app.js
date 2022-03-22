ca6d46eb82c1b06d98efa1dbb836ee7ded1c0a2e
asunfavour <asunfavour03@gmail.com>
2022-03-22 09:22:33 +0100

    using console.log to find the index of the largest number in the array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
 {
-    let stuff = [12, "name", function(){}, [1, 2, 3]]
+    let grades = [12, 4, 53, 898, 28, 84, 98, 88];
+    grades.length = 30;
+    grades[34] = 40;
+
+    let IndexLargest = grades[0];
+
+    for (let i = 0; i < grades.length; i++){
+        if(grades[i] > IndexLargest){
+            IndexLargest = grades[i]
+            //index of the largest number in the array
+            console.log(grades.indexOf(IndexLargest))
+        }
+    }
+    // console.log(IndexLargest + " index of " + grades[i]);
 }
