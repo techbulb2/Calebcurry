e0ffd5071ef5e250bc6f5f1af736f6a4f9113ed6
asunfavour <asunfavour03@gmail.com>
2022-03-22 09:41:38 +0100

    finding the average of a loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,33 @@
 {
-    let grades = [12, 4, 53, 898, 28, 84, 98, 88];
+    let grades = [12, 34, 52, 45, 35, 356, 98, 84, 783, 29];
     grades.length = 30;
     grades[34] = 40;
 
-    let IndexLargest = grades[0];
+    let count = 0;
+    let total = 0;
+
+    console.log(grades);
+
+    let largest = grades[0];
 
     for (let i = 0; i < grades.length; i++){
-        if(grades[i] > IndexLargest){
-            IndexLargest = grades[i]
-            //index of the largest number in the array
-            console.log(grades.indexOf(IndexLargest))
+        if (grades[i] !== undefined){
+            count++;
+            total += grades[i];
         }
+
+        if (grades[i] > largest){
+            
+            largest = grades[i];
+        }
+
+
     }
-    // console.log(IndexLargest + " index of " + grades[i]);
+
+
+    let avg = total / count;
+    console.log(avg);
+
+    console.log(largest)
+
 }
