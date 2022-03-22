d537d4396fa09b6ca2dc4bb79ee421a63b0c4748
asunfavour <asunfavour03@gmail.com>
2022-03-22 08:20:23 +0100

    multidimensional array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,3 @@
 {
-    let d = document.getElementById("destination")
-    for (let i = 0; i < 10; i++){
-        for (let k = i; k >= 0; k--){
-            d.append(k + "");
-        }
-        var br = document.createElement('br')
-        d.appendChild(br)
-    }
+    let stuff = [12, "name", function(){}, [1, 2, 3]]
 }
