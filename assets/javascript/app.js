769bec0061a0fc06b8e8db43c519c14462b9da8b
asunfavour <asunfavour03@gmail.com>
2022-03-26 09:36:52 +0100

    memotization and algorithm optimazation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,27 @@
 {
-    let myFunc = function pow(x, y){
+    pow.calculated = {}
+    function pow(x, y){
+        let stringVersion = x + "^" + y;
+        console.log("string version: " + stringVersion)
+
+        if(stringVersion in pow.calculated){
+            console.log("found!")
+            return pow.calculated[stringVersion]
+        }
+
+        console.log("calculating")
         let total = 1;
-        for(let i = 0; i < y; i++){
+        for (let i = 0; i < y; i++){
             total *= x;
         }
+        // pow.calculated.push(total)
+        pow.calculated[stringVersion] = total;
+        console.log(pow.calculated)
         return total;
     }
-    console.log(myFunc(3,3));
+
+    pow(3,3)
+    pow(3,3)
+    pow(4,3)
+    pow(6,3)
 }
