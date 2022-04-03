8b1020471cd99a487819f3bc385735549bd3d537
asunfavour <asunfavour03@gmail.com>
2022-04-03 06:08:21 +0100

    override in prototypal inheritance

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,20 @@
         active: true
     }
 
+    let student = {
+        major: "english"
+    }
+
     let teacher = {
         teaching: ["math", "science"]
     }
 
+    Object.setPrototypeOf(student, user)
+
     Object.setPrototypeOf(teacher, user)
 
-    console.log(teacher.active)
+    student.active = false;
+
+    console.log("teacher", teacher.active)
+    console.log("student", student.active)
 }
