4770477f21fdfcc1e45850618c90e28391dd93e7
asunfavour <asunfavour03@gmail.com>
2022-04-05 09:26:45 +0100

    css essentials

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,10 @@
 {
-    function User(){
-        this.active = false;
-    }
+    let myButton = document.getElementById("clickme")
+    myButton.style.backgroundColor = "red";
 
-    User.prototype.sayHello = function(){
-        return this.name + " says hi";
+    myButton.onclick = function(){
+        myButton.style.backgroundColor = "green";
+        myButton.disabled = true
     }
-
-    function Student(name, major){
-        this.name = name;
-        this.major = major;
-    }
-    Student.prototype = new User()
-    function Teacher(name, teaching){
-        this.name = name
-        this.teaching = teaching
-    }
-    Teacher.prototype = new User()
-    Teacher.prototype.sayHello = function(){
-        return "Teacher says hello"
-    }
-    let student = new Student("Peasant student", "English")
-
-    let teacher = new Teacher("caleb curry",  ["math", "science"])
-
-    console.log(student, teacher)
-
-    console.log(teacher instanceof Teacher)
-    console.log(teacher instanceof User)
-
-    if(Teacher instanceof Student){
-        console.log("yup")
-    }else {
-        console.log("nope")
-    }
-
-    function doSomething(user){
-        if (user instanceof User){
-            return 5;
-        }
-        return -1;
-    }
-    console.log(doSomething("teacher"))
 }
+
