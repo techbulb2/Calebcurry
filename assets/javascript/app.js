60c92b52846287e78aeb51550c41d534cced8acf
asunfavour <asunfavour03@gmail.com>
2022-04-05 14:13:36 +0100

    DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
 {
-    let myButton = document.getElementById("clickme")
-    myButton.style.backgroundColor = "red";
+    let list = document.childNodes[1].childNodes[2].childNodes[9];
 
-    myButton.onclick = function(){
-        myButton.style.backgroundColor = "green";
-        myButton.disabled = true
-    }
+    console.log(list)
+
+    console.log(list.parentElement)
+    console.log(list.nextSibling.nextSibling)
 }
-
